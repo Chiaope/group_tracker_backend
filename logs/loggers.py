7697091fa26d3f63d9381aade77923931f9d79e7
Chiaope <gui_kang@hotmail.com>
2024-12-25 13:01:09 +0800

    updated logger config

--- logs/loggers.py
+++ logs/loggers.py
@@ -1,25 +1,5 @@
 import logging
 import os
-from main import app_name
-
-# logger = logging.getLogger(app_name)
-# logger.setLevel(logging.DEBUG)
-
-# formatter = logging.Formatter(
-#     '[%(asctime)s] - %(module)s - %(levelname)s - %(message)s')
-
-# log_path = os.path.join(os.path.dirname(__file__), 'root.log')
-# fh = logging.FileHandler(log_path)
-# fh.setLevel(logging.DEBUG)
-
-# ch = logging.StreamHandler()
-# ch.setLevel(logging.NOTSET)
-
-# ch.setFormatter(formatter)
-# fh.setFormatter(formatter)
-
-# logger.addHandler(ch)
-# logger.addHandler(fh)
 
 
 LOG_CONFIG = {
@@ -47,7 +27,7 @@ LOG_CONFIG = {
     },
     'formatters': {
         'std': {
-            'format': '[%(asctime)s] - %(module)s - %(levelname)s - %(message)s'
+            'format': '[%(asctime)s] [%(levelname)s] [%(module)s] - %(message)s'
         },
 
     }
